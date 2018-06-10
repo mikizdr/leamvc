6ae7187482e7eb7061ed5f259aef642ef0c213d3
mikizdr <miroslav.zdravkovic@gmail.com>
2018-06-10 21:54:53 +0200

    Core class prepareation for controllers

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -13,14 +13,31 @@ class Core
 
     public function __construct()
     {
-        $this->getUrl();
+        // echo json_encode($this->getUrl());
+        $url = $this->getUrl();
+
+        // Look in controllers for first value
+        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            // If exists, set as controller
+            $this->currentController = cwords($url[0]);
+            // Unset 0 index
+            unset($url[0]);
+        }
+
+        // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
     }
 
     public function getUrl()
     {
-        echo $_GET['url'];
-        // if () {
-        //     # code...
-        // }
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
